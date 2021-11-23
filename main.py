cb9489be23833f6618e40488a3a14faff25baab6
Raivis Rasčevskis <94110450+raivisrascevskis@users.noreply.github.com>
2021-11-23 11:38:00 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ print(f'Mans vārds ir Jānis')
 print(Mans vārds ir Ričards)
 print("Mans vārds ir Laura")
 print("Mans vārds ir Laimonis")
+print(f'Mans vārds ir Raivis')
 print("Pievienoju jaunu vārdu")
+
+
 
 
